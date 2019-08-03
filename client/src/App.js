b4284ae894d7e1d5c1200edb2b77ccb69691b127
Devansh Sharma <devanshsharma289@gmail.com>
2019-08-02 23:29:38 -0400

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [myData, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/data")
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <ul>
+        {myData.map(customer => (
+          <p>{customer.name}</p>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = 1000;
+
+app.get("/data", (req, res) => {
+  const customers = [{ id: 1, name: "John" }, { id: 2, name: "Bill" }];
+  res.json(customers);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});

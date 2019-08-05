061ef53efd7743c4c75adff56e66cbc56f554c7c
Devansh Sharma <devanshsharma289@gmail.com>
2019-08-04 23:27:54 -0400

    Added MongoDB support

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,42 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
+const axios = require('axios');
+
 function App() {
   const [myData, setData] = useState([]);
 
   useEffect(() => {
-    fetch("/data")
+    /*fetch("/data")
       .then(response => response.json())
       .then(data => {
         setData(data);
-      });
+      });*/
+    
+      axios.post('/data', )
   }, []);
+
+  const addItem = () => {
+    useEffect(() => {
+      fetch("/data", {
+        headers: {
+          'Accept': "application/json"
+          'Content-Type' : "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          setData(data);
+        });
+    }, []);
+  };
 
   return (
     <div className="App">
       <button />
       <ul>
         {myData.map(customer => (
-          <p>{customer.name}</p>
+          <p>{customer.hlo}</p>
         ))}
       </ul>
     </div>
--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const mongo = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3003;
 
+const url = "mongodb://localhost:27017";
+
 app.get("/data", (req, res) => {
-  const customers = [{ id: 1, name: "John" }, { id: 2, name: "Bill" }];
-  res.json(customers);
+  //const customers = [{ id: 1, name: "John" }, { id: 2, name: "Bill" }];
+  //res.json(customers);
+
+  mongo.connect(url, (err, client) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const db = client.db("userData");
+    const collection = db.collection("userData");
+
+    var date = new Date();
+    var current_hour = date.getTime();
+    //collection.insertOne({ hlo: current_hour });
+
+    collection.find().toArray((err, items) => {
+      res.json(items);
+    });
+  });
 });
 
 app.listen(PORT, () => {
